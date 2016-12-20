2e5237c2f90846d77f7673c83f8358b41313ec9c
Bibek <bibek.thapahere@gmail.com>
2016-12-20 02:03:45 -0800

    Added the content to the database

--- app/src/main/java/com/example/android/chatapp1/MainActivity.java
+++ app/src/main/java/com/example/android/chatapp1/MainActivity.java
@@ -1,13 +1,96 @@
 package com.example.android.chatapp1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.android.chatapp1.com.example.android.chatapp1.model.ChatMessageModel;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SIGN_IN_REQUEST_CODE = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_REQUEST_CODE);
+        } else {
+
+            Toast.makeText(this, "Welcome to " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName(), Toast.LENGTH_LONG).show();
+        }
+        displayChatMessages();
+
+        FloatingActionButton fab=(FloatingActionButton)findViewById(R.id.fb1);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input=(EditText)findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push().setValue(new ChatMessageModel(input.getText().toString(),FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+            input.setText("");
+            }
+        });
+
+    }
+
+    private void displayChatMessages() {
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Signed in Successfully", Toast.LENGTH_LONG).show();
+                displayChatMessages();
+
+            }
+            else
+            {
+                    Toast.makeText(this,"We couldnot sign you . Plz try later",Toast.LENGTH_LONG).show();
+                finish();
+            }
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.menu_sign_out)
+        {
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                 Toast.makeText(MainActivity.this,"You have been signed out",Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            });
+
+        }
+        return true;
     }
 }
+
+
---
+++ app/src/main/java/com/example/android/chatapp1/com/example/android/chatapp1/model/ChatMessageModel.java
@@ -0,0 +1,47 @@
+package com.example.android.chatapp1.com.example.android.chatapp1.model;
+
+
+import java.util.Date;
+
+public class ChatMessageModel {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public ChatMessageModel(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        // Initialize to current time
+        messageTime = new Date().getTime();
+    }
+
+    public ChatMessageModel(){
+
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+}
